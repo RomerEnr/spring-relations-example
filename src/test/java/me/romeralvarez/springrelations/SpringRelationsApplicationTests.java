937118b5d4e9bdb5b8854174bb0876131f7312ca
romerenriquealvarezmartinez <romeralvareziv@gmail.com>
2023-08-31 15:11:04 +0100

    First commit

---
+++ src/test/java/me/romeralvarez/springrelations/SpringRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.romeralvarez.springrelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
